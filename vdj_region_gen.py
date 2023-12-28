c30691b1811044f37de140ddf435ef6b31e0d81f
joycezhu28 <128271250+joycezhu28@users.noreply.github.com>
2023-12-27 20:58:02 -0800

    Update vdj_region_gen.py
    Improved both methods of v_trimming

--- vdj_region_gen.py
+++ vdj_region_gen.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from difflib import SequenceMatcher
+from Bio import Align
 import argparse
 pd.options.mode.chained_assignment = None  # default='warn'
 
@@ -57,6 +58,25 @@ def id_match(vj, v, j):
     new_df['J_AA_Seq'] = j_seq_list
     return new_df
 
+#Next two functions are used in v_trimming()
+
+#Find all common substrings between two strings
+def all_common_substrings(str1, str2):
+    common_substrings = set()
+
+    for i in range(len(str1)):
+        for j in range(len(str2)):
+            k = 0
+            while i + k < len(str1) and j + k < len(str2) and str1[i + k] == str2[j + k]:
+                k += 1
+                common_substrings.add(str1[i:i + k])
+
+    return list(common_substrings)
+
+#Filter common substrings that start with 'C' and are at least 2 characters long
+def filter_substrings(substrings):
+    return [substring for substring in substrings if substring.startswith('C') and len(substring) >= 2]
+
 #Finds longest common sequence between V & CDR3 and keeps only the preceding V sequence
 #Requires longest common sequence to start with a 'C'
 def v_trimming(v_seq, cdr3_seq):
@@ -66,48 +86,44 @@ def v_trimming(v_seq, cdr3_seq):
         cdr3_seq = CDR3 sequence
     Output:
         v_seq_trimmed = identify lcs between V and CDR3 sequences, locate in V sequence, and trim corresponding end off of V sequence
-            1st method: lcs starting with C and with match size of > 2
+            1st method: lcs starting with C and with match size of >= 2
             2nd method: pairwise alignment starting with C and with match size of >= 5
     '''
-    match = SequenceMatcher(None, v_seq[-25:], cdr3_seq).find_longest_match()
-    match_seq = v_seq[-25:][match.a:match.a + match.size]
-    if (match_seq[0] == 'C') & (match.size > 2):
+    # Method 1: Longest Common Subsequence (LCS)
+    possible_substrings = filter_substrings(all_common_substrings(v_seq[-20:], cdr3_seq))
+    if possible_substrings:
+        match_seq = max(possible_substrings, key=len)
         v_seq_as_parts = v_seq.split(match_seq)[:-1]
         v_seq_trimmed = match_seq.join(v_seq_as_parts)
     else:
-        v_segment = v_seq[-25:]
-        #Pairwise alignment
-        alignments = PairwiseAligner().align(v_segment, cdr3_seq)
-        alignment = alignments[0]
+        # Method 2: Pairwise Alignment
+        v_segment = v_seq[-20:]
+        aligner = Align.PairwiseAligner()
+        aligner.mode = 'global'
 
-        #Retrieving indexes/coordinates of matches and organizing it as a dictionary
-        paths = alignment.coordinates
-        v_path = list(paths[0])
-        cdr3_path = list(paths[1])
-        path_dict = {cdr3_path[i]: v_path[i] for i in range(len(cdr3_path))}
+        #Set scoring parameters
+        aligner.match_score = 2
+        aligner.mismatch_score = -1
+        aligner.open_gap_score = -2
+        aligner.extend_gap_score = -0.5
 
-        #Extract detected common sequence from pairwise alignment
-        new_match_seq = v_segment[path_dict[0]:]
+        #Perform alignment
+        alignments = aligner.align(v_segment, cdr3_seq)
 
-        #Ensure common sequence doesn't have a false start (as seen in specific cases)
-        test_segment = new_match_seq[1:]
-        test_segment2 = new_match_seq[2:]
-        #Calculating alignment scores
-        PairwiseAligner().open_gap_score = 0
-        PairwiseAligner().extend_gap_score = 0
-        align_score = PairwiseAligner().score(new_match_seq, cdr3_seq)
-        test_align_score = PairwiseAligner().score(test_segment, cdr3_seq)
-        test_align_score2 = PairwiseAligner().score(test_segment2, cdr3_seq)
-        if (align_score == test_align_score):
-            new_match_seq = test_segment
-        if (align_score == test_align_score2):
-            new_match_seq = test_segment2
-
-        #Ensure start is C and alignment score >= 5
-        if (new_match_seq[0] == 'C') & (align_score >= 5):
+        #Find alignments starting with 'C' and then the one with the greatest match score
+        threshold = 0
+        filtered_alignments = [
+            alignment for alignment in alignments
+            if (v_segment[alignment.aligned[0][0][0]].startswith('C'))  #Alignment starts with 'C'
+            and (alignment.score >= threshold) #Alignment score meets threshold set above
+        ]
+        try:
+            best_alignment = max(filtered_alignments, key=lambda x: x.score)
+            #Extract trimmed V sequence
+            new_match_seq = v_segment[best_alignment.aligned[0][0][0]:]
             v_seq_as_parts = v_seq.split(new_match_seq)[:-1]
             v_seq_trimmed = new_match_seq.join(v_seq_as_parts)
-        else:
+        except:
             v_seq_trimmed = 'NA'
     return v_seq_trimmed
 
@@ -128,8 +144,16 @@ def j_trimming(j_seq):
             value_list.append(value)
     value_string = ''.join(str(num) for num in value_list)
     match = SequenceMatcher(None, value_string, '2101').find_longest_match()
-    string_start = match.a + 1
-    j_seq_trimmed = j_seq[string_start:]
+    if match.size == 4:
+        string_start = match.a + 1
+        j_seq_trimmed = j_seq[string_start:]
+    else:
+        match = SequenceMatcher(None, value_string, '2111').find_longest_match()
+        if match.size == 4:
+            string_start = match.a + 1
+            j_seq_trimmed = j_seq[string_start:]
+        else:
+            j_seq_trimmed = 'NA'
     return j_seq_trimmed
 
 #Applies v_trimming and j_trimming functions to create full region sequence
@@ -148,12 +172,17 @@ def full_region_gen(vj):
             v_trimmed = v_trimming(v,c)
         except:
             v_trimmed = 'NA'
-        j_trimmed = j_trimming(j)
+        try:
+            j_trimmed = j_trimming(j)
+        except:
+            j_trimmed = 'NA'
         v_trim_list.append(v_trimmed)
         j_trim_list.append(j_trimmed)
     vj['V_AA_Trimmed'] = v_trim_list
     vj['J_AA_Trimmed'] = j_trim_list
-    vj = vj[vj.V_AA_Trimmed != 'NA']
+    dropout_threading = vj[(vj.V_AA_Trimmed == 'NA') | (vj.J_AA_Trimmed == 'NA')].reset_index()
+    dropout_threading.to_csv('dropout_threading.csv', header=True, index=False)
+    vj = vj[(vj.V_AA_Trimmed != 'NA') & (vj.J_AA_Trimmed != 'NA')]
     vj['V_CDR3_J_Sequence'] = vj['V_AA_Trimmed'] + vj['CDR3'] + vj['J_AA_Trimmed']
     return vj
 
@@ -189,6 +218,8 @@ def vdj_region_gen(vj_file, v_file, j_file, output):
     vj_with_seq = id_match(vj_df, v_df, j_df)
     #Remove rows where AA sequence for VID could not be found
     vj_with_seq2 = vj_with_seq[(vj_with_seq['V_AA_Seq'] != 'NA') & (vj_with_seq['J_AA_Seq'] != 'NA')]
+    dropout_seq = vj_with_seq[(vj_with_seq['V_AA_Seq'] == 'NA') | (vj_with_seq['J_AA_Seq'] == 'NA')].reset_index()
+    dropout_seq.to_csv('dropout_VIDmatch.csv', header=True, index=False)
     #Generate column containing full V-CDR3-J AA sequences
     final_df = full_region_gen(vj_with_seq2)
     #Save output to csv
